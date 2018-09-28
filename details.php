bee4d9aa31d715ea4d14d78083ac832deadf943a
Changa Guan <changa_1994@hotmail.com>
2018-09-28 17:53:16 -0300

    archivos nuevos

--- details.php
+++ details.php
@@ -2,12 +2,21 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Detalle</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
+    <!-- Header -->
+    <?php include('main-header.php') ?>
+    <!-- --------------- -->
+
+    <!-- Menu-Bar -->
+    <?php include('main-nav.php') ?>
+    <!-- --------------- -->
+
+    <!-- Contenido-->
     <section class="container">
       <article class="row">
         <article class="col-md-8">
@@ -43,10 +52,17 @@
       </article>
       </article>
     </section>
+    <!-- --------------- -->
 
+    <!-- Aside-Bar -->
+    <?php include('aside.php') ?>
+    <!-- --------------- -->
 
+    <!-- Header -->
+    <?php include('main-footer.php') ?>
+    <!-- --------------- -->
 
-
-
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
